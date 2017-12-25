a2083709f94bb54ecbd01667c8a3d068d1bdf794
Bogdan <bogdan@product-test.ru>
2017-12-25 11:43:49 +0300

    del

--- src/test/java/Assert/DatabaseConnection.java
+++ src/test/java/Assert/DatabaseConnection.java
@@ -3,6 +3,7 @@ package Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.sql.*;
+import static ;
 
 public class DatabaseConnection {
 
@@ -11,7 +12,10 @@ public class DatabaseConnection {
     static Connection conn = null;
 
     public static Statement SQLConnect() throws SQLException {
-
+        String hostName = getHostName();
+        String dbName = getDbName();
+        String user = getUser();
+        String password = getPassword();
 
         String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
 
---
+++ src/test/java/MyConfig.java
@@ -0,0 +1,22 @@
+public class MyConfig {
+    static String hostName = "mq5to1dak2.database.windows.net";
+    static String dbName = "pt_db_01.12.17_15.11.17";
+    static String user = "producttest@mq5to1dak2";
+    static String password = "FloSVOtLL5PiBoW";
+
+    public static String getDbName() {
+        return dbName;
+    }
+
+    public static String getHostName() {
+        return hostName;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+}
